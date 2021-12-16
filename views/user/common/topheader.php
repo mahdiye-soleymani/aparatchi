866ca8a65e4b311e3b1bbc474d11f7f6684b7517
mahdiye <you@example.com>
2021-12-16 20:48:03 +0330

    wellcome message and exit from site

--- views/user/common/topheader.php
+++ views/user/common/topheader.php
@@ -1,27 +1,32 @@
-
 <header id="main_header">
-
     <!----start header--->
     <div id="header">
 
         <!----start logo--->
         <div id="logo">
-            <a href="<?php echo AddressMyWebsite;?>"><img src="<?php echo AddressMyWebsite;?>/public/user/images/logo.PNG"></a>
+            <a href="<?php echo AddressMyWebsite; ?>"><img
+                        src="<?php echo AddressMyWebsite; ?>/public/user/images/logo.png"></a>
         </div>
         <!----End logo--->
 
         <!----start header_row_top_right--->
         <div id="header_row_top_right" class="BYekan-sm">
             <div id="header_row_top">
-                <span class="bg_span_login style_span_header_top"></span>
-                <a href="<?php echo  AddressMyWebsite.'login'?>"> وارد شوید</a>
-                <span class="bg_span_register style_span_header_top"></span>
-                <a href="<?php echo  AddressMyWebsite.'register'?>">ثبت نام کنید</a>
+                <?php if (isset($_SESSION['emailmember'])) { ?>
+                    <a style="font-size: 15px;color: #00a65a"> کاربر گرامی به وبسایت خوش امدید</a>
 
+                    <a href="<?php echo AddressMyWebsite.'exitsite'?>" style="color: #721c24;font-size: 15px;margin-right: 10px;border:solid 2px;padding: 3px ">خروج از وبسایت</a>
+                <?php } else { ?>
+                    <span class="bg_span_login style_span_header_top"></span>
+                    <a href="<?php echo AddressMyWebsite . 'login' ?>"> وارد شوید</a>
+                    <span class="bg_span_register style_span_header_top"></span>
+                    <a href="<?php echo AddressMyWebsite . 'register' ?>">ثبت نام کنید</a>
+                <?php } ?>
             </div>
             <div id="header_row_bottom">
                 <div id="search_box">
-                    <input class="BYekan-sm" type="text" name="search_box" placeholder="فیلم مورد نظرتان را جستجو کنید....">
+                    <input class="BYekan-sm" type="text" name="search_box"
+                           placeholder="فیلم مورد نظرتان را جستجو کنید....">
                     <span></span>
                 </div>
             </div>
@@ -30,4 +35,3 @@
     </div><!----End Main_header--->
 
 </header>
-
---
+++ views/user/users/users_content.php
@@ -0,0 +1,60 @@
+<div class="content">
+    <div class="user_order_status">
+        <div class="order_status">
+            <div class="line_dash">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+            <ul>
+                <li class="active">
+                    <span class="circle"></span>
+                    <span class="line_span_acount"></span>
+                    <span class="title_span_acount">ورود به سایت</span>
+                </li>
+                <li>
+                    <span class="circle"></span>
+                    <span class="line_span_acount"></span>
+                    <span class="title_span_acount">اطلاعات سفارش</span>
+                </li>
+                <li>
+                    <span class="circle"></span>
+                    <span class="line_span_acount"></span>
+                    <span class="title_span_acount">بررسی سفارش</span>
+                </li>
+                <li>
+                    <span class="circle"></span>
+                    <span class="title_span_acount">اطلاعات پرداخت</span>
+                </li>
+            </ul>
+            <div class="line_dash line_dash_two">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </div>
+    </div>
+
+    <div class="user_status">
+        <div class="user_status_right">
+            <div class="register_header">
+                <img src="<?php echo AddressMyWebsite.'public/user/'?>images/register_page.png">
+                <h4>عضو سایت هستید ؟</h4>
+                <p>برای تکمیل فرایند خرید خود وارد شوید</p>
+                <a href="<?php echo AddressMyWebsite;?>login" class="btn_login_user">وارد شوید</a>
+            </div>
+        </div>
+        <div class="user_status_left">
+            <div class="register_header">
+                <img src="<?php echo AddressMyWebsite.'public/user/'?>images/register_page.png">
+                <h4>تازه وارد هستید؟</h4>
+                <p>برای تکمیل فرایند خرید ثبت نام کنید</p>
+                <a href="<?php echo AddressMyWebsite;?>register" class="btn_register_user">ثبت نام می کنم</a>
+            </div>
+        </div>
+    </div>
+</div>
